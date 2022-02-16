5d417ab7f8e7e11b57d98dd497053022fe1994de
Apurva Dani <apurvadani@Apurvas-MacBook-Air.local>
2022-02-15 21:55:44 -0800

    Deploy Ready

--- execute.sh
+++ execute.sh
@@ -13,4 +13,4 @@ echo "tmpdir for the job: "$TMPDIR
 echo "total gpu resources allocated: "$CUDA_VISIBLE_DEVICES 
 echo "CPU allocated: "$(taskset -c -p $$)
 pwd
-python train_wrn_ebm.py --lr .0001 --dataset cifar10 --optimizer adam --p_x_weight 1.0 --p_y_given_x_weight 1.0 --p_x_y_weight 0.0 --sigma .03 --width 10 --depth 28 --save_dir //nas/home/adani/projects/adversarial/apurva_jem/JEM/outdir --plot_uncond --warmup_iters 1000
+python train_wrn_ebm.py --lr .0001 --dataset cifar10 --optimizer adam --p_x_weight 1.0 --p_y_given_x_weight 1.0 --p_x_y_weight 0.0 --sigma .03 --width 10 --depth 28 --save_dir /nas/home/adani/projects/adversarial/apurva_jem/JEM/outdir --plot_uncond --warmup_iters 1000
